2ab7ca6275fdad9533e0fbeaf577cdfd419b21e6
Ryan Haig <haigryan@gmail.com>
2020-06-03 17:21:37 -0600

    creating heroku build

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: deno run --allow-net=:${PORT} --cached-only --unstable server.ts --port=${PORT}
--- server.ts
+++ server.ts
@@ -1,8 +1,15 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts'
+import * as flags from 'https://deno.land/std/flags/mod.ts'
 
 import db from './db/config.ts'
 import router from './routes.ts'
 import querySanitizer from './utils/querySanitizer.ts'
+
+// get env port
+const { args } = Deno
+const argPort = flags.parse(args).port
+const DEFAULT_PORT = 4000
+const port = argPort ? Number(argPort) : DEFAULT_PORT
 
 const app = new Application()
 await db.connect() // <- this is the error
@@ -11,6 +18,5 @@ app.use(querySanitizer)
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-const port = 4000
 console.log(`🦕 Deno 🦕 Server running on port 🦖 ${ port } 🦖`)
 await app.listen({ port })
